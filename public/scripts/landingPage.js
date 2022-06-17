81a28d01a87ab08e45b9c171cc426ad776a155ef
Julian Scarramberg <scarrambergj@gmail.com>
2022-06-17 19:27:22 -0300

    Cambios LandingPage.js

--- public/scripts/landingPage.js
+++ public/scripts/landingPage.js
@@ -1,15 +1,33 @@
+
+  
+ 
+
 let cotizaciones = [];
 
-
-
-
-addEventListener('load', fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
-    .then(data => data.json())
+let traer = () => {
+  fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
+    .then(res => res.json())
     .then(data => {
-    for (const i of data) {
-        cotizaciones.push(i);
+        for (const i of data) {
+            cotizaciones.push(i)
+            
         }
-}))
+    
+    });
+};
+
+traer();
+
+function algo() {
+  for (const i of cotizaciones) {
+    console.log(i.casa)    
+  }
+}
+
+algo();
+
+
+
 
 
 
