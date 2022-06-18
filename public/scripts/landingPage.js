2e9b876038732b30058d1104ad404f1029172931
Julian Scarramberg <scarrambergj@gmail.com>
2022-06-17 22:02:17 -0300

    Cambios en script

--- public/scripts/landingPage.js
+++ public/scripts/landingPage.js
@@ -1,30 +1,64 @@
+let cotizaciones;
+let cotizacion = $('#cotizacion');
+let fecha = new Date().toLocaleString();
 
-  
- 
-
-let cotizaciones = [];
-
-let traer = () => {
-  fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
+let traer = async () => {
+    await fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
     .then(res => res.json())
     .then(data => {
-        for (const i of data) {
-            cotizaciones.push(i)
-            
-        }
-    
-    });
+          cotizaciones = data.filter(element => {
+          return element.casa.nombre == 'Dolar Oficial' || 
+          element.casa.nombre == 'Dolar Blue' ||
+          element.casa.nombre == 'Dolar Contado con Liqui' ||
+          element.casa.nombre == 'Dolar' ||
+          element.casa.nombre == 'Dolar Bolsa' ||
+          element.casa.nombre == 'Dolar turista'
+        })
+       });
 };
 
-traer();
 
-function algo() {
+let cargar = async () => {
+  traer();
+  await traer();
+  enviar();
+}
+
+cargar();
+
+let enviar = () => {
   for (const i of cotizaciones) {
-    console.log(i.casa)    
+    let nombre = i.casa.nombre;
+    cotizacion.append(`<div class="col-4 my-3">
+    <div class="px-5 cotizacion">
+    <div class="row">
+      <img src="/public/images/dolaroficial.png">
+      <h1> ${nombre.toUpperCase()}</h1>
+    </div>
+    <div class="row">
+      <div class="col">
+        <p>COMPRA</p>
+        <p> ${i.casa.compra} </p>
+      </div>
+      <div class="col">
+        <p>VENTA</p>
+        <p> ${i.casa.venta} </p>
+      </div>
+    </div>
+    <div class="row">
+      <p>VARIACION</p>
+      <p> ${i.casa.variacion} </p>
+    </div>
+    <div class="row">
+      <p>ACTUALIZADO: ${fecha}</p>
+    </div>
+    </div>       
+  </div>`)
   }
 }
 
-algo();
+
+  
 
 
 
@@ -32,9 +66,23 @@ algo();
 
 
 
-
-
-
-
-
-
+/* <div class="col-sm w-75" id="cotizacion">
+          <div class="row">
+            <img src="/public/images/dolaroficial.png">
+            <h1>DOLAR OFICIAL</h1>
+          </div>
+          <div class="row">
+            <div class="col">
+              <p>COMPRA</p>
+            </div>
+            <div class="col">
+              <p>VENTA</p>
+            </div>
+          </div>
+          <div class="row">
+            <p>VARIACION</p>
+          </div>
+          <div class="row">
+            <p>ACTUALIZADO:</p>
+          </div>       
+        </div> */
